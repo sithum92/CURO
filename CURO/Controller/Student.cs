6e38e329f0c1d04848f18717646a1899b2d79f58
Sithum Witharana <witharana92@gmail.com>
2022-07-26 17:47:05 +0530

    Create Student Entity

--- CURO/Controller/Student.cs
+++ CURO/Controller/Student.cs
@@ -8,6 +8,22 @@ namespace CURO.Controller
 {
   public class Student
     {
+        public int Stu_ID { get; set; }
+        public string Stu_Name { get; set; }
+
+        public string Stu_Address { get; set; }
+        public string Stu_WhatsApp { get; set; }
+        public string Stu_Mobile { get; set; }
+        public string Stu_Per_Mobile { get; set; }
+        public Double Stu_Reg_Fee { get; set; }
+        public string Stu_Pre_Name { get; set; }
+        public string Stu_Email { get; set; }
+        public string Stu_School { get; set; }
+        public string Stu_CardNo { get; set; }
+        public int Stu_Al_Year { get; set; }
+        public string Stu_NIC { get; set; }
+        public DateTime Stu_Reg_Date { get; set; }
+
 
     }
 }
