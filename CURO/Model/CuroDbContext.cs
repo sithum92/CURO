ea7b8a7a140b288dc87e830a319ecdda28a432b3
Sithum Witharana <witharana92@gmail.com>
2022-07-26 09:55:53 +0530

    Add project files.

---
+++ CURO/Model/CuroDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using CURO.Controller;
+
+namespace CURO.Model
+{
+   public class CuroDbContext:DbContext
+    {
+        public DbSet<Student> Student { get; set; }
+
+    }
+}
